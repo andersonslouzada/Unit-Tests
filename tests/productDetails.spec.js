c214cc0717008acc5320268dc4221607c2981903
andersonlouzada <anderson.s.louzada@gmail.com>
2022-12-02 10:27:40 -0300

    cria primeiro test para função productDetails

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -30,14 +30,23 @@ const productDetails = require('../src/productDetails');
 */
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
-  it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    fail('Teste vazio!');
-    // ESCREVA SEUS TESTES ABAIXO:
-    // Teste se productDetails é uma função.
-    // Teste se o retorno da função é um array.
-    // Teste se o array retornado pela função contém dois itens dentro.
-    // Teste se os dois itens dentro do array retornado pela função são objetos.
-    // Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.
-    // Teste se os dois productIds terminam com 123.
+  it('Verifica se `productDetails` é uma função', () => {
+    expect(typeof productDetails).toBe('function');
+    expect(typeof productDetails).not.toBe('object');
   });
+  it('Verifica se `productDetails` retorna uma array', () => {
+  });
+  // Teste se o retorno da função é um array.
+  it('Verifica se `productDetails` retorna uma array', () => {
+  });
+  // Teste se o array retornado pela função contém dois itens dentro.
+  it('Verifica se `productDetails` retorna uma array', () => {
+  });
+  // Teste se os dois itens dentro do array retornado pela função são objetos.
+  it('Verifica se `productDetails` retorna uma array', () => {
+  });
+  // Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.
+  it('Verifica se `productDetails` retorna uma array', () => {
+  });
+  // Teste se os dois productIds terminam com 123.
 });
