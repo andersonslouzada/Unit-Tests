e229f8cb3f0a183fbc05dd18f722cd296db18873
andersonlouzada <anderson.s.louzada@gmail.com>
2022-12-05 11:12:47 -0300

    testa se a função getCharacter retorna undefined se não receber parâmetro

--- tests/getCharacter.spec.js
+++ tests/getCharacter.spec.js
@@ -43,14 +43,28 @@ Retorno:
 */
 
 describe('9 - Implemente os casos de teste da função `getCharacter`', () => {
-  it('Verifica se a função `getCharacter` retorna o objeto do personagem corretamente.', () => {
-    fail('Teste vazio!');
-    // ESCREVA SEUS TESTES ABAIXO:
-    // 1. Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+  it('Verifica se a função `getCharacter` , quando não recebe nenhum parâmetro, retorna undefined.', () => {
+    expect(getCharacter()).toBe(undefined);
+  })
+  it('Verifica se a função `getCharacter` , quando não recebe nenhum parâmetro, retorna undefined.', () => {
+    expect(getCharacter()).toBe(undefined);
+  })
     // 2. Teste se a função retorna o objeto correto para o parâmetro 'Arya',
+  it('Verifica se a função `getCharacter` , quando não recebe nenhum parâmetro, retorna undefined.', () => {
+    expect(getCharacter()).toBe(undefined);
+  })
     // 3. Teste se a função retorna o objeto correto para o parâmetro 'Brienne',
+  it('Verifica se a função `getCharacter` , quando não recebe nenhum parâmetro, retorna undefined.', () => {
+    expect(getCharacter()).toBe(undefined);
+  })
     // 4. Teste se a função retorna o objeto correto para o parâmetro 'Melissandre',
+  it('Verifica se a função `getCharacter` , quando não recebe nenhum parâmetro, retorna undefined.', () => {
+    expect(getCharacter()).toBe(undefined);
+  })
     // 5. Teste se o parâmetro não é Case Sensitive, ou seja, independente de conter letras maiúsculas ou minúsculas retorna o mesmo objeto relativo a ele.
+  it('Verifica se a função `getCharacter` , quando não recebe nenhum parâmetro, retorna undefined.', () => {
+    expect(getCharacter()).toBe(undefined);
+  })  
     // 6. Teste se ao passar um nome que não está na tabela, a função retorna undefined.
-  });
+  ;
 });
